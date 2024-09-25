0a81997fdac918faed1f56adf132eb81f4ee7523
NawadeepKhatri123 <5086950@nwpolytech.ca>
2024-09-25 13:14:33 -0600

    added comments

--- Prog2/Prog2.cpp
+++ Prog2/Prog2.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 int main(){
-      double x{};
-      double y{};
+      double x{};    // creates a double variable x
+      double y{};    // creates a double variable y
 
       std::cout << "ENTER A DOUBLE VALUE FOR X : ";
-      std::cin >> x;
+      std::cin >> x;                                    // new input for x
       std::cout << "ENTER A DOUBLE VALUE FOR Y : ";
-      std::cin >> y;
+      std::cin >> y;                                    //new input for y
 
-      double* ptr1 = &x;
-      double* ptr2 = &y;
+      double* ptr1 = &x;                              // ptr1 points at x
+      double* ptr2 = &y;                              // ptr2 points at y
 
-      std::cout<<" x is "<<x<<" and y is "<<y<<"and the sum is "<<*ptr1+*ptr2<<std::endl;
-
+      std::cout<<" x is "<<x<<" and y is "<< y <<"and the sum is "<<*ptr1+*ptr2<<std::endl;
+      // so the contents inside the addresses that ptr1 & ptr2 is summed and printed.
 }
