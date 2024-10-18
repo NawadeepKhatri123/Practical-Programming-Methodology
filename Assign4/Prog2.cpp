8b1d4ddfa518a83a36ba3e6361a1b02d2363c931
NawadeepKhatri123 <5086950@nwpolytech.ca>
2024-10-18 15:01:12 -0600

    Crap game

--- Assign4/Prog2.cpp
+++ Assign4/Prog2.cpp
@@ -1,81 +1,90 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <thread>     // For std::this_thread::sleep_for
 #include <chrono>
+
 void playGame();
-void point();
 int die();
-int credits {100};
+int credits{100};
 int wager{};
 
-int main( ) {
+int main() {
     char ans;
-
     bool done = false;
-    while ( ! done ) {
-          while(true){
-              std::cout << " you have " << credits << " how much do you want to wager? ";
-              std::cin >> wager;
-              if ( wager > credits || wager < 0 ) {
-                std::cout << " your credits is out of range. Please try again. \n";
-              }else {
-                credits -= wager;
-                break;
-              }
-           }
-      playGame();   // YOU MUST WRITE THIS FUNCTION
-      std::cout << " Play again (y/n) ? ";
-      std::cin >> ans;
-      if ( ans == 'n' || ans == 'N') done = true;
-      else done = false;
+
+    std::srand(static_cast<unsigned int>(std::time(0))); // Seed random number generator
+
+    while (!done) {
+        playGame();   // Call the playGame function
+        std::cout << "Play again (y/n)? ";
+        std::cin >> ans;
+        if (ans == 'n' || ans == 'N') {
+            done = true;
+        } else {
+            done = false;
+        }
     }
     return 0;
 }
 
 void playGame() {
-    int sum = die();
-    if ( sum == 7 || sum == 11 ) {
-      std::cout<<"You won! " << wager << " Your new balance is "<< (wager*2) + credits<< '\n' ;
-      credits = credits + (wager * 2);
-      return;
-    }
-    else if ( sum == 2 || sum == 3 || sum == 12 ) {
-        std::cout<<"You Lose! " << wager << " credits. Your new balance is" << credits <<'\n';
-        return;
-    }
-    else if (sum == 4 || sum == 5 || sum == 6 || sum == 8 || sum == 9 || sum == 10 ) {std::cout<<" point is " << sum << '\n';point();return;}
+    while (true) {
+        std::cout << "You have " << credits << " credits. How much do you want to wager? ";
+        std::cin >> wager;
 
+        if (wager > credits) {
+            std::cout << "Your wager exceeds your balance. Try again.\n";
+            continue; // Ask for the wager again
+        } else if (wager <= 0) {
+            std::cout << "Wager must be greater than 0. Try again.\n";
+            continue; // Ask for the wager again
+        }
+
+        credits -= wager; // Deduct the wager from credits
+        int sum = die(); // Roll the dice
+
+        if (sum == 7 || sum == 11) {
+            std::cout << "Player rolled " << sum << " - You win!\n";
+            credits += wager * 2; // Player wins double the wager
+            std::cout << "You won " << wager << " credits. Your new balance is " << credits << " credits.\n";
+            break; // Exit the game
+        } else if (sum == 2 || sum == 3 || sum == 12) {
+            std::cout << "Player rolled " << sum << " - You lose.\n";
+            std::cout << "You lost " << wager << " credits. Your new balance is " << credits << " credits.\n";
+            break; // Exit the game
+        } else {
+            std::cout << "Point is " << sum << '\n';
+            int point = sum; // Set the point
+            while (true) {
+                sum = die(); // Keep rolling the dice
+
+                if (sum == 7) {
+                    std::cout << "Player rolled " << sum << " - You lose.\n";
+                    std::cout << "You lost " << wager << " credits. Your new balance is " << credits << " credits.\n";
+                    return; // Exit the game
+                } else if (sum == point) {
+                    std::cout << "Player rolled " << sum << " - You win!\n";
+                    credits += wager * 2; // Player wins double the wager
+                    std::cout << "You won " << wager << " credits. Your new balance is " << credits << " credits.\n";
+                    return; // Exit the game
+                }
+            }
+        }
+
+        // Check if the player is out of credits
+        if (credits <= 0) {
+            std::cout << "You are out of credits - game over!\n";
+            exit(0); // End the game
+        }
+    }
 }
-
-void point(){
-  int sum{};
-  while(true){
-    sum = die();
-    if ( sum == 7 ){
-        std::cout<<"You Lose! " << wager << " credits. Your new balance is" << credits <<'\n';
-        return;
-    }
-    else if(sum == 4 || sum == 5 || sum == 6 || sum == 8 || sum == 9 || sum == 10 ) {
-        std::cout<<"You won! " << wager << " Your new balance is "<< (wager*2) + credits<< '\n' ;
-        credits = credits + (wager * 2);
-        return;
-    }
-
-  }
-
-}
-   
-
-
 
 int die() {
-    std::srand(std::time(0));
-
-    int die1 = (std::rand() % 6) + 1;
-    std::this_thread::sleep_for(std::chrono::seconds(2));  // Delay for 3 seconds
-
-    int die2 = (std::rand() % 6) + 1;
-    std::cout<< "player rolled " << die1 <<" + " << die2 << " = " << die1+die2 << '\n';
-    return die1+die2;
+    int die1 = (std::rand() % 6) + 1; // Roll first die
+    int die2 = (std::rand() % 6) + 1; // Roll second die
+    std::this_thread::sleep_for(std::chrono::seconds(1)); // Delay for 1 second
+    std::cout << "Player rolled " << die1 << " + " << die2 << " = " << die1 + die2 << '\n';
+    return die1 + die2; // Return the sum of the dice
 }
+
