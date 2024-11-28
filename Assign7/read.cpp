8f63abc04a3058024046865484c7e8d358422b67
NawadeepKhatri123 <5086950@nwpolytech.ca>
2024-11-28 14:52:50 -0700

    open a file

---
+++ Assign7/read.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(){
+
+	std::ifstream myfile ("file.txt");
+	if (myfile.is_open()){
+		
+
+	
+	std::cout<<"this me nawadeep alright";
+	}
+
+}
